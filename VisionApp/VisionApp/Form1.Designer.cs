0f831d04ae465039dc05075613925a601063902e
LEEINAH <22na7592@gmail.com>
2025-12-26 12:48:11 +0900

    좌우 반전&저장

--- VisionApp/VisionApp/Form1.Designer.cs
+++ VisionApp/VisionApp/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnOpen = new System.Windows.Forms.Button();
             this.btnGray = new System.Windows.Forms.Button();
+            this.btnFlip = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,7 +40,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(259, 271);
+            this.pictureBox1.Size = new System.Drawing.Size(344, 271);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -55,7 +57,7 @@
             // 
             // btnGray
             // 
-            this.btnGray.Location = new System.Drawing.Point(161, 299);
+            this.btnGray.Location = new System.Drawing.Point(132, 299);
             this.btnGray.Name = "btnGray";
             this.btnGray.Size = new System.Drawing.Size(110, 31);
             this.btnGray.TabIndex = 2;
@@ -63,11 +65,33 @@
             this.btnGray.UseVisualStyleBackColor = true;
             this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
             // 
+            // btnFlip
+            // 
+            this.btnFlip.Location = new System.Drawing.Point(132, 336);
+            this.btnFlip.Name = "btnFlip";
+            this.btnFlip.Size = new System.Drawing.Size(105, 34);
+            this.btnFlip.TabIndex = 3;
+            this.btnFlip.Text = "좌우 반전";
+            this.btnFlip.UseVisualStyleBackColor = true;
+            this.btnFlip.Click += new System.EventHandler(this.btnFlip_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(248, 299);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(108, 31);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "저장하기";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(283, 350);
+            this.ClientSize = new System.Drawing.Size(368, 380);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnFlip);
             this.Controls.Add(this.btnGray);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.pictureBox1);
@@ -83,6 +107,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnFlip;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- VisionApp/VisionApp/Form1.cs
+++ VisionApp/VisionApp/Form1.cs
@@ -48,8 +48,42 @@ namespace VisionApp
             // 2. 흑백 변환 알고리즘 적용
             Cv2.CvtColor(originalImg, grayImg, ColorConversionCodes.BGR2GRAY);
 
-            // 3. 변환된 이미지를 PictureBox에 바로 업데이트
-            pictureBox1.Image = BitmapConverter.ToBitmap(grayImg);
+            // 3. 원본을 흑백으로 변경
+            originalImg = grayImg;
+
+            // 4. 변환된 이미지를 PictureBox에 바로 업데이트
+            pictureBox1.Image = BitmapConverter.ToBitmap(originalImg);
+        }
+
+        private void btnFlip_Click(object sender, EventArgs e)
+        {
+            if (originalImg == null) return;
+
+            Mat flipImg = new Mat();
+            // 1: 좌우 반전, 0: 상하 반전, -1: 좌우&상하 반전
+            Cv2.Flip(originalImg, flipImg, FlipMode.Y);
+
+            // 화면 업데이트 (원본을 반전된 것으로 교체하고 싶다면 아래처럼 하면 된다)
+            originalImg = flipImg;
+            pictureBox1.Image = BitmapConverter.ToBitmap(originalImg);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+
+            using (SaveFileDialog saveFile = new SaveFileDialog())
+            {
+                saveFile.Filter = "JPG 파일|*.jpg|PNG 파일|*.png|모든 파일|*.*";
+
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    // PictureBox에 있는 현재 이미지를 Mat으로 다시 바꿔서 저장하거나
+                    // 현재 originalImg 변수를 저장한다.
+                    Cv2.ImWrite(saveFile.FileName, originalImg);
+                    MessageBox.Show("저장되었습니다!");
+                }
+            }
         }
     }
 }
