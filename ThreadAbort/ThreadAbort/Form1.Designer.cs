d74fa0ea5e58500c91eb968a738688de03c9c1c0
LEEINAH <22na7592@gmail.com>
2025-12-29 13:43:43 +0900

    Abort, Join

---
+++ ThreadAbort/ThreadAbort/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ThreadAbort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.Location = new System.Drawing.Point(43, 59);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(151, 73);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnAbort.Location = new System.Drawing.Point(258, 59);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(144, 73);
+            this.btnAbort.TabIndex = 1;
+            this.btnAbort.Text = "Abort";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click);
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnJoin.Location = new System.Drawing.Point(470, 59);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(143, 73);
+            this.btnJoin.TabIndex = 2;
+            this.btnJoin.Text = "Join";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(39, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 245);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnJoin);
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnAbort;
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ThreadAbort/ThreadAbort/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadAbort
+{
+    public partial class Form1 : Form
+    {
+        Thread threadWorker;
+        bool isRunning = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            isRunning = true;
+            threadWorker = new Thread(RunThreadTimer);
+            threadWorker.Start();            
+        }
+
+        private void btnAbort_Click(object sender, EventArgs e)
+        {
+            threadWorker.Abort();
+        }
+
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+
+            threadWorker.Join();
+        }
+
+        private void RunThreadTimer()
+        {
+            int count = 0;
+            while (isRunning)
+            {
+                if (label1.IsHandleCreated)
+                {
+                    label1.Invoke(new MethodInvoker(delegate
+                    {
+                        if (count > 10)
+                        { 
+                            label1.Text = string.Empty;
+                            count = 0;
+                        
+                        }
+                        else
+                        {
+                            label1.Text += ".";
+                            count++;
+                        }
+                    })); 
+                }             
+               Thread.Sleep(100);
+            }
+            Thread T = new Thread(() =>
+            {
+                if (label1.IsHandleCreated)
+                {
+                    label1.Invoke(new MethodInvoker(delegate
+                    {
+                        label1.Text = "끝";
+                    }));
+                }
+            });
+            T.Start();           
+        }
+    }
+}
