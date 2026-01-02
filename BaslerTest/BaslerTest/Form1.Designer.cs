d3c947682b9ef351f7e26b056f35d4cb917b0a5e
LEEINAH <22na7592@gmail.com>
2026-01-02 17:24:04 +0900

    카메라 연결

---
+++ BaslerTest/BaslerTest/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BaslerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnectGrab = new System.Windows.Forms.Button();
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnectGrab
+            // 
+            this.btnConnectGrab.Location = new System.Drawing.Point(107, 333);
+            this.btnConnectGrab.Name = "btnConnectGrab";
+            this.btnConnectGrab.Size = new System.Drawing.Size(112, 35);
+            this.btnConnectGrab.TabIndex = 0;
+            this.btnConnectGrab.Text = "Connect";
+            this.btnConnectGrab.UseVisualStyleBackColor = true;
+            this.btnConnectGrab.Click += new System.EventHandler(this.btnConnectGrab_Click);
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(26, 27);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(282, 275);
+            this.pictureBoxDisplay.TabIndex = 1;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 394);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.btnConnectGrab);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnectGrab;
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+    }
+}
+
---
+++ BaslerTest/BaslerTest/Form1.cs
@@ -0,0 +1,77 @@
+﻿using Basler.Pylon;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace BaslerTest
+{
+    public partial class Form1 : Form
+    {
+        private Camera camera = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnectGrab_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 첫 번째 사용 가능한 카메라 디바이스를 생성.
+                camera = new Camera();
+                MessageBox.Show("Using device: " + camera.CameraInfo[CameraInfoKey.ModelName]);
+
+                // 카메라를 연다.
+                camera.Open();
+
+                // 이미지를 한 장만 가져오도록 설정한다 (One-shot grab).
+                // 연속 촬영 (Continuous shot)은 별도 설정이 필요하다.
+                camera.Parameters[PLCamera.AcquisitionMode].SetValue(PLCamera.AcquisitionMode.SingleFrame);
+
+                // 이미지 획득 시작.
+                camera.StreamGrabber.Start();
+
+                // 이미지 그랩 결과(grab result)를 가져옴.
+                IGrabResult grabResult = camera.StreamGrabber.RetrieveResult(5000, TimeoutHandling.ThrowException);
+
+                using (grabResult)
+                {
+                    // 이미지 획득이 성공했는지 확인.
+                    if (grabResult.GrabSucceeded)
+                    {
+                        // 그랩된 이미지를 Bitmap으로 변환하여 PictureBox에 표시.
+                        // 이 과정에서는 pylon의 이미지 포맷 변환 유틸리티를 사용하는 것이 좋다.
+                        Bitmap bitmap = new Bitmap(grabResult.Width, grabResult.Height, PixelFormat.Format32bppRgb);
+                        // 실제 구현에서는 Basler.Pylon.ImageFormatConverter 클래스를 사용하여 안전하게 변환해야 함.
+
+                        // PictureBox에 이미지 표시 (실제 구현 시 변환 코드 추가 필요)
+                        pictureBoxDisplay.Image = bitmap;
+                        MessageBox.Show("Image grabbed successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error grabbing image: " + grabResult.ErrorCode + " " + grabResult.ErrorDescription);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                // 그랩 중지 및 카메라 닫기
+                if (camera != null)
+                {
+                    camera.StreamGrabber.Stop();
+                    camera.Close();
+                    camera.Dispose();
+                    camera = null;
+                }
+            }
+        }
+    }
+}
