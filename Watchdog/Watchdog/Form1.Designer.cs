3e7ea5063d6926898ec10700c6448186ab1f10de
LEEINAH <22na7592@gmail.com>
2026-02-02 09:45:08 +0900

    Watchdog

---
+++ Watchdog/Watchdog/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Watchdog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.gbConfig = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSaveInterval = new System.Windows.Forms.Button();
+            this.cbPath = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFindPath = new System.Windows.Forms.Button();
+            this.tbInterval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gbStatus = new System.Windows.Forms.GroupBox();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.niWatchdog = new System.Windows.Forms.NotifyIcon(this.components);
+            this.gbConfig.SuspendLayout();
+            this.gbStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbLog
+            // 
+            this.lbLog.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.ItemHeight = 16;
+            this.lbLog.Location = new System.Drawing.Point(480, 38);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(512, 308);
+            this.lbLog.TabIndex = 0;
+            // 
+            // gbConfig
+            // 
+            this.gbConfig.Controls.Add(this.label1);
+            this.gbConfig.Controls.Add(this.btnSaveInterval);
+            this.gbConfig.Controls.Add(this.cbPath);
+            this.gbConfig.Controls.Add(this.label4);
+            this.gbConfig.Controls.Add(this.btnFindPath);
+            this.gbConfig.Controls.Add(this.tbInterval);
+            this.gbConfig.Controls.Add(this.label2);
+            this.gbConfig.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbConfig.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.gbConfig.Location = new System.Drawing.Point(24, 171);
+            this.gbConfig.Name = "gbConfig";
+            this.gbConfig.Size = new System.Drawing.Size(430, 175);
+            this.gbConfig.TabIndex = 1;
+            this.gbConfig.TabStop = false;
+            this.gbConfig.Text = "Config";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "(ms)";
+            // 
+            // btnSaveInterval
+            // 
+            this.btnSaveInterval.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSaveInterval.FlatAppearance.BorderSize = 0;
+            this.btnSaveInterval.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.btnSaveInterval.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveInterval.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnSaveInterval.Location = new System.Drawing.Point(169, 119);
+            this.btnSaveInterval.Name = "btnSaveInterval";
+            this.btnSaveInterval.Size = new System.Drawing.Size(70, 25);
+            this.btnSaveInterval.TabIndex = 9;
+            this.btnSaveInterval.Text = "Save";
+            this.btnSaveInterval.UseVisualStyleBackColor = false;
+            this.btnSaveInterval.Click += new System.EventHandler(this.btnSaveInterval_Click);
+            // 
+            // cbPath
+            // 
+            this.cbPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbPath.FormattingEnabled = true;
+            this.cbPath.Location = new System.Drawing.Point(20, 62);
+            this.cbPath.Margin = new System.Windows.Forms.Padding(0);
+            this.cbPath.Name = "cbPath";
+            this.cbPath.Size = new System.Drawing.Size(350, 25);
+            this.cbPath.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Path";
+            // 
+            // btnFindPath
+            // 
+            this.btnFindPath.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnFindPath.FlatAppearance.BorderSize = 0;
+            this.btnFindPath.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.btnFindPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFindPath.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnFindPath.Location = new System.Drawing.Point(376, 62);
+            this.btnFindPath.Name = "btnFindPath";
+            this.btnFindPath.Size = new System.Drawing.Size(35, 25);
+            this.btnFindPath.TabIndex = 5;
+            this.btnFindPath.Text = "...";
+            this.btnFindPath.UseVisualStyleBackColor = false;
+            this.btnFindPath.Click += new System.EventHandler(this.btnFindPath_Click);
+            // 
+            // tbInterval
+            // 
+            this.tbInterval.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbInterval.Location = new System.Drawing.Point(20, 119);
+            this.tbInterval.Margin = new System.Windows.Forms.Padding(0);
+            this.tbInterval.Name = "tbInterval";
+            this.tbInterval.Size = new System.Drawing.Size(100, 25);
+            this.tbInterval.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Check Interval";
+            // 
+            // gbStatus
+            // 
+            this.gbStatus.Controls.Add(this.pbStatus);
+            this.gbStatus.Controls.Add(this.btnOff);
+            this.gbStatus.Controls.Add(this.btnOn);
+            this.gbStatus.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbStatus.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.gbStatus.Location = new System.Drawing.Point(24, 19);
+            this.gbStatus.Name = "gbStatus";
+            this.gbStatus.Size = new System.Drawing.Size(220, 134);
+            this.gbStatus.TabIndex = 2;
+            this.gbStatus.TabStop = false;
+            this.gbStatus.Text = "Monitoring Status";
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.BackColor = System.Drawing.Color.Gainsboro;
+            this.pbStatus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(222)))), ((int)(((byte)(133)))));
+            this.pbStatus.Location = new System.Drawing.Point(20, 89);
+            this.pbStatus.Margin = new System.Windows.Forms.Padding(0);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(176, 23);
+            this.pbStatus.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbStatus.TabIndex = 2;
+            // 
+            // btnOff
+            // 
+            this.btnOff.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnOff.FlatAppearance.BorderSize = 0;
+            this.btnOff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOff.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOff.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnOff.Location = new System.Drawing.Point(121, 34);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(75, 35);
+            this.btnOff.TabIndex = 1;
+            this.btnOff.TabStop = false;
+            this.btnOff.Text = "OFF";
+            this.btnOff.UseVisualStyleBackColor = false;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            this.btnOff.MouseEnter += new System.EventHandler(this.btnOff_MouseEnter);
+            this.btnOff.MouseLeave += new System.EventHandler(this.btnOff_MouseLeave);
+            // 
+            // btnOn
+            // 
+            this.btnOn.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnOn.FlatAppearance.BorderSize = 0;
+            this.btnOn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOn.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOn.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnOn.Location = new System.Drawing.Point(20, 34);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(75, 35);
+            this.btnOn.TabIndex = 0;
+            this.btnOn.TabStop = false;
+            this.btnOn.Text = "ON";
+            this.btnOn.UseVisualStyleBackColor = false;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            this.btnOn.MouseEnter += new System.EventHandler(this.btnOn_MouseEnter);
+            this.btnOn.MouseLeave += new System.EventHandler(this.btnOn_MouseLeave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(477, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Log";
+            // 
+            // niWatchdog
+            // 
+            this.niWatchdog.Icon = ((System.Drawing.Icon)(resources.GetObject("niWatchdog.Icon")));
+            this.niWatchdog.Text = "Watchdog Running";
+            this.niWatchdog.Visible = true;
+            this.niWatchdog.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.niWatchdog_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1023, 377);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.gbStatus);
+            this.Controls.Add(this.gbConfig);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbConfig.ResumeLayout(false);
+            this.gbConfig.PerformLayout();
+            this.gbStatus.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.GroupBox gbConfig;
+        private System.Windows.Forms.Button btnFindPath;
+        private System.Windows.Forms.TextBox tbInterval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox gbStatus;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbPath;
+        private System.Windows.Forms.Button btnSaveInterval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.NotifyIcon niWatchdog;
+    }
+}
+
---
+++ Watchdog/Watchdog/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Watchdog
+{
+    public partial class Form1 : Form
+    {
+        string targetApp;
+        int checkInterval;
+        bool isMonitoring = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Rectangle rect = new Rectangle(1, 1, pbStatus.Width - 2, pbStatus.Height - 2);
+            pbStatus.Region = new Region(rect);
+
+            string defaultPath = @"E:\Vision PG\DW_Sealer_Single\DW_Sealer_Single C 20260128 A\DW_Sealer_Single\bin\x64\Release\DW_Sealer_Single.exe";
+
+            if (File.Exists(defaultPath))
+            {
+                SetTargetApp(defaultPath);
+                return;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 사용자가 Off 버튼을 누르지 않았고, 감시 중이라면 종료 대신 숨기기
+            if (isMonitoring && e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true; 
+                this.Hide();     
+            }
+            else
+            {
+                isMonitoring = false;
+                niWatchdog.Visible = false;
+                niWatchdog.Dispose();
+                Environment.Exit(0);
+            }
+        }
+
+        private void niWatchdog_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show(); // 폼 다시 보이기
+            this.WindowState = FormWindowState.Normal; // 최소화 되어있다면 원래대로
+            this.Activate(); // 창 활성화
+        }
+
+        private void btnFindPath_Click(object sender, EventArgs e)
+        {      
+            // 파일 탐색기 객체 생성
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // 초기 설정
+                openFileDialog.InitialDirectory = "E:\\"; // 처음 열릴 폴더 위치
+                openFileDialog.Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*"; // 실행 파일만 보이게 필터링
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    SetTargetApp(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            if (isMonitoring) return; // 이미 감시 중이면 중복 실행 방지
+
+            // 경로 확인
+            if (cbPath.Text.Length == 0)
+            {
+                MessageBox.Show("경로를 선택해주세요.");
+                cbPath.Focus();
+                return;
+            }
+
+            // 감시 주기 확인
+            if (tbInterval.Text.Length == 0)
+            {
+                MessageBox.Show("감시 주기를 입력해주세요.");
+                tbInterval.Focus();
+                return;
+            }
+            else
+            {
+                // 입력값 확인
+                if (!int.TryParse(tbInterval.Text, out int result))
+                {
+                    MessageBox.Show("숫자만 입력해 주세요.");
+                    tbInterval.Focus();
+                    return;
+                }
+                this.checkInterval = result;
+            }
+
+            isMonitoring = true;
+
+            // 버튼 색 변경
+            btnOn.BackColor = Color.LightGreen;
+            btnOn.FlatAppearance.BorderColor = Color.LightGreen;
+            btnOff.BackColor = Color.WhiteSmoke;
+            btnOff.FlatAppearance.BorderColor = Color.WhiteSmoke;
+
+            // 프로그레스 바
+            pbStatus.Maximum = 100;
+            pbStatus.Value = 0;
+            pbStatus.ForeColor = Color.FromArgb(98, 222, 133);
+
+            Task.Run(() =>
+            {             
+                while (isMonitoring)
+                {
+                    Process[] processes = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(targetApp));
+                    if (processes.Length == 0)
+                    {
+                        try
+                        {
+                            // 감시 시작
+                            Process.Start(targetApp);
+
+                            // 로그 찍기
+                            WriteLog("▶  프로그램이 재시작 되었습니다.");
+                        }
+                        catch (Exception ex) { Console.WriteLine(ex.Message); }
+                    }
+
+                    int elapsed = 0;
+                    int step = 100; // 업데이트 간격 (0.1초마다 게이지 갱신)
+
+                    while (elapsed < this.checkInterval && isMonitoring)
+                    {
+                        Thread.Sleep(step);
+                        elapsed += step;
+                        
+                        int percent = (int)((double)elapsed / this.checkInterval * 100);
+
+                        // 100을 넘지 않게 방어 코드
+                        if (percent > 100) percent = 100;
+
+                        this.Invoke(new MethodInvoker(delegate {
+                            if (!this.IsDisposed) pbStatus.Value = percent;
+                        }));
+                    }
+                    Thread.Sleep(200);
+                }
+            });            
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            if (!isMonitoring)
+            {
+                MessageBox.Show("감시 중이 아닙니다.");
+                return;
+            }
+
+            isMonitoring = false;
+            pbStatus.Value = 0; // 게이지 초기화
+
+            // 색 변경
+            btnOff.BackColor = Color.LightCoral;
+            btnOff.FlatAppearance.BorderColor = Color.LightCoral;
+            btnOn.BackColor = Color.WhiteSmoke;
+            btnOn.FlatAppearance.BorderColor = Color.WhiteSmoke;
+            pbStatus.ForeColor = Color.Gainsboro;
+
+            // 로그 찍기
+            WriteLog("▶  프로그램 감시가 중지 되었습니다.");
+        }
+
+        // 파일 경로 등록
+        private void SetTargetApp(string filePath)
+        {
+            cbPath.Text = filePath;
+            this.targetApp = filePath;
+
+            // 화면엔 파일명만, 파일엔 전체 경로 저장
+            WriteLog($"▶ 경로가 설정되었습니다. [{Path.GetFileName(filePath)}]", filePath);
+        }
+
+        // 감시 주기 변경
+        private void btnSaveInterval_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(tbInterval.Text, out int result))
+            {
+                this.checkInterval = result;
+
+                // 로그 찍기
+                WriteLog($"▶  감시 주기가 {result}ms 로 설정되었습니다.");
+            }
+            else
+            {
+                MessageBox.Show("숫자만 입력해 주세요.");
+                tbInterval.Focus();
+                return;
+            }
+        }
+
+        // 로그 작성
+        private void WriteLog(string message, string detail = "")
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(() => WriteLog(message)));
+                return;
+            }
+
+            lbLog.Items.Add($"{DateTime.Now:yyyy-MM-dd HH : mm : ss} : {message}");
+
+            string fileLog = string.IsNullOrEmpty(detail) ? message : $"{message} ({detail})";
+            SaveLogToFile(fileLog);
+
+            if (lbLog.Items.Count > 0)
+            {
+                lbLog.SelectedIndex = lbLog.Items.Count - 1;
+                lbLog.TopIndex = lbLog.Items.Count - 1;
+            }
+        }
+
+        // 로그 저장
+        private void SaveLogToFile(string message)
+        {
+            try
+            {
+                // 로그 파일 경로 설정
+                string folderPath = Path.Combine(Application.StartupPath, "Logs");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath); // 폴더가 없으면 생성
+                }
+
+                // 파일명 설정
+                string fileName = DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+                string filePath = Path.Combine(folderPath, fileName);
+
+                // 로그 내용 구성 (시간 : 메시지)
+                string logContent = $"{DateTime.Now:yyyy-MM-dd HH : mm : ss} : {message}{Environment.NewLine}";
+
+                // 파일에 이어쓰기
+                File.AppendAllText(filePath, logContent);
+            }
+            catch (Exception ex)
+            {
+                // 파일 쓰기 실패 시 콘솔에만 출력
+                Console.WriteLine("로그 저장 실패: " + ex.Message);
+            }
+        }        
+
+        //---------- 버튼 디자인 ---------- 
+        private void btnOn_MouseEnter(object sender, EventArgs e)
+        {
+            if (btnOn.BackColor == Color.WhiteSmoke)
+                btnOn.BackColor = Color.LightGray;
+            else
+            {
+                btnOn.BackColor = btnOn.BackColor;
+                btnOn.FlatAppearance.BorderColor = btnOn.BackColor;
+            }                
+        }
+
+        private void btnOn_MouseLeave(object sender, EventArgs e)
+        {
+            if (btnOn.BackColor == Color.LightGray)
+                btnOn.BackColor = Color.WhiteSmoke;
+        }
+
+        private void btnOff_MouseEnter(object sender, EventArgs e)
+        {
+            if (btnOff.BackColor == Color.WhiteSmoke)
+                btnOff.BackColor = Color.LightGray;
+            else
+            {
+                btnOff.BackColor = btnOff.BackColor;
+                btnOff.FlatAppearance.BorderColor = btnOff.BackColor;
+            }                
+        }
+
+        private void btnOff_MouseLeave(object sender, EventArgs e)
+        {
+            if (btnOff.BackColor == Color.LightGray)
+                btnOff.BackColor = Color.WhiteSmoke;
+        }        
+    }
+}
